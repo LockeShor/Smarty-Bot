4e907bbfb0e78addd1cf2f72c0fc9c4b6d350aaf
Redpug111 <mrdoodly111@gmail.com>
2020-12-15 08:18:08 -0500

    new commit system and reaction roles

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
-set name=new commit system and reaction roles
-git commit -am "%name%"
+git add .
+git commit -am "new commit system and reaction roles"
 git rm -rf --cached .
 git add .
 git push origin main
