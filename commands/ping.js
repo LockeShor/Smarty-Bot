fc20ad181e746dd36cc30f0e271557d3f8d2e1e0
Redpug111 <mrdoodly111@gmail.com>
2020-12-15 08:12:44 -0500

    commit name here

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: "this is a ping command!",
-    execute(message, args, client){
-        message.channel.send('pong!');
-        console.log(`ping was ponged`);
-        client.channels.cache.get('788065072898768976').send(`ping was ponged`);
-    }
-}
--- push.bat
+++
@@ -1,4 +0,0 @@
-set name=commit name here
-git add .
-git commit -am "%name%"
-git push origin main
